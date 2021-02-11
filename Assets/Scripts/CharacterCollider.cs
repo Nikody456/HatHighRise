5f89a3be910510528c1ae9001f33d26b077b7117
Steve Datz <sfdatz@live.com>
2021-02-11 12:47:04 -0600

    HatPickups and animator for CharacterView

---
+++ Assets/Scripts/CharacterCollider.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Statistics;
+///we should start a namespace Character
+
+
+[RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class CharacterCollider : MonoBehaviour
+{
+    [SerializeField] CharacterView _characterView = default;
+    [SerializeField] Stats _characterStats = default;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Debug.Log($"{this.gameObject.name} Trigger2D with {collision.gameObject.name} ");
+        Hat hat = collision.GetComponent<Hat>();
+        if(hat)
+        {
+            hat.OnPickup(_characterStats, _characterView);
+        }
+    }
+
+}
--- Assets/Scripts/Hats/Hat.cs
+++ Assets/Scripts/Hats/Hat.cs
@@ -3,37 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 using Statistics;
 
+[RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(SpriteRenderer))]
 public class Hat : MonoBehaviour
 {
     [SerializeField] HatData _hatData = default;
+    SpriteRenderer _spriteRenderer;
 
     public Modifier Modifier { get; private set; }
+
 
     /*********INIT******************************************************************************************************/
 
     void Awake()
     {
+        _spriteRenderer = this.GetComponent<SpriteRenderer>();
         SetUpModifier();
     }
 
     void SetUpModifier()
     {
         Modifier = new Modifier(_hatData.Stat, _hatData.Type, _hatData.Value);
+        _spriteRenderer.sprite = _hatData.Image;
     }
 
     /***************************************************************************************************************/
 
-    public void OnPickup(Stats stats)
+    public void OnPickup(Stats stats, CharacterView view)
     {
         stats.AddModifier(Modifier);
-        ///TODO tell the characterView to wear this
+        /// tell the characterView to wear this
+        view.PickUpHat(this);
     }
 
 
-    public void OnPutDown(Stats stats)
+    public void OnPutDown(Stats stats, CharacterView view)
     {
         stats.RemoveModifier(Modifier);
-        ///TODO tell the characterView to remove this
+        /// tell the characterView to remove this
+        view.PutDownHat(this);
     }
 
     /***************************************************************************************************************/

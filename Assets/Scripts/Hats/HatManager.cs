9ad759f4a9430f11e3b594df8649c9fe7efeb2ed
Steve Datz <sfdatz@live.com>
2021-02-16 18:22:32 -0600

    Hacked in pixel detection

--- Assets/Scripts/Hats/HatManager.cs
+++ Assets/Scripts/Hats/HatManager.cs
@@ -9,6 +9,8 @@ public class HatManager : MonoBehaviour
     private float _characterHeight = 0.5f;
     private float _yOffset = 0.25f;
 
+    [SerializeField] SpriteRenderer _characterSpriteHACK;
+
     public void OnPickUpHat(Hat hat)
     {
         //Debug.Log("I am picking up a hat");
@@ -29,10 +31,49 @@ public class HatManager : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.K))
+        var sprite = _characterSpriteHACK.sprite;
+        //Texture2D characterFrameSprite = new Texture2D((int)sprite.rect.width, (int)sprite.rect.height);
+        Color[] pixels = sprite.texture.GetPixels(
+                    (int)sprite.textureRect.x,
+                    (int)sprite.textureRect.y,
+                    (int)sprite.textureRect.width,
+                    (int)sprite.textureRect.height);
+
+
+        string blackIndexs = "";
+        int indexLast = 0;
+        for (int i = 0; i < pixels.Length; ++i)
         {
-            //OnPutDownHat(_hats[0]);
+
+            if (pixels[i] == Color.black)
+            {
+                blackIndexs += i + " , ";
+                indexLast = i;
+
+                print($"First BlackPixel Seen is at Horizontal:  {indexLast / (int)sprite.textureRect.width} and yHeight:{indexLast / (int)sprite.textureRect.height}");
+                return;
+            }
         }
+
+        print(blackIndexs);
+        print($"Division: {indexLast / (int)sprite.textureRect.width} vs {indexLast / (int)sprite.textureRect.height}");
+        print($"Mod: {indexLast % (int)sprite.textureRect.width} vs {indexLast % (int)sprite.textureRect.height}");
+
+      //  print($" {pixels.Length} Pixel are : {(int)sprite.textureRect.width} , {(int)sprite.textureRect.height}");
+
+
+        //characterFrameSprite.SetPixels(pixels);
+        // characterFrameSprite.Apply();
+
+
+        //Vector3 size = new Vector3(32, 32, 32);
+        //int x = Mathf.FloorToInt(transform.position.x / size.x * characterFrameSprite.width);
+        //int z = Mathf.FloorToInt(transform.position.z / size.z * characterFrameSprite.height);
+        //Vector3 pos = transform.position;
+        // pos.y = characterFrameSprite.GetPixel(x, z).grayscale * size.y;
+        // transform.position = pos;
+
+        // print($"Wtf isthis : {characterFrameSprite.GetPixel(x, z).grayscale * size.y}");
 
     }
 

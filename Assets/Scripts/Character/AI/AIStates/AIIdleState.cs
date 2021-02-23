e4914010d1245749d185c1e6ca796e6f6affcbd9
Steve Datz <sfdatz@live.com>
2021-02-23 17:06:42 -0600

    basic follow AI

---
+++ Assets/Scripts/Character/AI/AIStates/AIIdleState.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace AI
+{
+    public class AIIdleState : AIState
+    {
+        AIInput _ai;
+        
+        public AIIdleState(AIInput ai)
+        {
+            _ai = ai;
+        }
+
+        public override bool CanExit(eAIStates nextState)
+        {
+            return true;
+        }
+        public override void OnDisable(Transform target)
+        {
+
+        }
+        public override void OnEnable(Transform target)
+        {
+            _ai.SetMovement(0);
+        }
+
+        public override void Execute(Transform target)
+        {
+            if(!CheckExitConditions(target))
+            {
+                _ai.SetMovement(0);
+            }
+        }
+
+        protected virtual bool CheckExitConditions(Transform target)
+        {
+            //Debug.Log($"Dis= {Vector3.Distance(_ai.transform.position, target.position)}");
+            if (Vector3.Distance(_ai.transform.position, target.position) < _ai.DetectionRange)
+            {
+                _ai.SetState(eAIStates.MOVE);
+                return true;
+
+            }
+
+            return false;
+        }
+
+    }
+}
---
+++ Assets/Scripts/Character/AI/AIStates/AIState.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace AI
+{
+    public abstract class AIState
+    {
+
+        public enum eAIStates { IDLE, MOVE, ATTACK, JUMP, DEATH}
+
+        public abstract void OnEnable(Transform target);
+
+        public abstract void OnDisable(Transform target);
+
+        public abstract void Execute(Transform target);
+
+        public abstract bool CanExit(eAIStates nextState);
+    }
+}

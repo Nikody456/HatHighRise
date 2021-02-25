3aabd4d8786f960f29f30159584f72ab7b5e5d82
JadenSchneider <schneiderj3185@my.uwstout.edu>
2021-02-25 16:09:08 -0600

    Wall Jumping and Coyote Time

--- Assets/Scripts/Character/CharMovement.cs
+++ Assets/Scripts/Character/CharMovement.cs
@@ -18,6 +18,14 @@ public class CharMovement : MonoBehaviour
 
     [SerializeField] float gravity = default; ///TODO ??: Move into its own GAMECONSTANTS static class thats non-monobehavior
     [SerializeField] float friction = default;///TODO ??: Move into its own GAMECONSTANTS static class thats non-monobehavior
+    [SerializeField] float airFriction = default;///TODO ??: Move into its own GAMECONSTANTS static class thats non-monobehavior
+
+    //COYOTE TIME VARS
+    [SerializeField] float coyoteTime;
+    private bool _coyotePreEnabled = false;
+    private float _coyotePre = 10;
+    private bool _coyotePostEnabled = false;
+    private float _coyotePost = 10;
 
     [SerializeField] int jumpLimit = 1; //Additional jump powerup?
     private int _jumps;
@@ -30,7 +38,6 @@ public class CharMovement : MonoBehaviour
 
     private void Start()
     {
-
         _playerStats = GetComponent<Stats>();
         _view = GetComponent<CharacterView>();
         _controller = GetComponent<Rigidbody2D>();
@@ -41,8 +48,15 @@ public class CharMovement : MonoBehaviour
     {
         if (isGrounded())
         {
-            _currentSpeed = _playerStats.CurrentMoveSpeed * sprintSpeed;
-            sprintParticles.Play();
+            _currentSpeed = _playerStats.CurrentSprintSpeed;
+            if (Mathf.Abs(input) > .25f)
+            {
+                sprintParticles.Play();
+            }
+            else
+            {
+                sprintParticles.Stop();
+            }
         }
         else
         {
@@ -60,7 +74,7 @@ public class CharMovement : MonoBehaviour
     public void TryJump()
     {
 
-        if(_jumps == 0 && isGrounded())
+        if(_jumps == 0 && (isGrounded() || isOnWall() != 0 || _coyotePost <= coyoteTime))
         {
             DoJump();
         }
@@ -68,12 +82,31 @@ public class CharMovement : MonoBehaviour
         {
             DoJump();
         }
+        else if (!_coyotePreEnabled)
+        {
+            _coyotePre = 0;
+            _coyotePreEnabled = true;
+        }
 
     }
 
     private void DoJump()
     {
-        _controller.velocity = new Vector2(_controller.velocity.x, _playerStats.CurrentJumpSpeed);
+
+        _coyotePre = coyoteTime+1;
+        _coyotePreEnabled = false;
+
+        int wallCheck = isOnWall();
+        if (wallCheck != 0 && !isGrounded())
+        {
+            moveDirection = wallCheck * _currentSpeed;
+            _controller.velocity = new Vector2(moveDirection, _playerStats.CurrentJumpSpeed);
+        }
+        else
+        {
+            _controller.velocity = new Vector2(_controller.velocity.x, _playerStats.CurrentJumpSpeed);
+        }
+        
         sprintParticles.Emit(20);
         _jumps++;
     }
@@ -85,7 +118,7 @@ public class CharMovement : MonoBehaviour
 
     bool isGrounded()
     {
-        Vector2 boxPosition = new Vector2(transform.position.x, transform.position.y - transform.localScale.y / 2 - .065f);
+        Vector2 boxPosition = new Vector2(transform.position.x, transform.position.y - transform.GetComponent<BoxCollider2D>().size.y / 2 - .065f);
         Collider2D[] collisions = Physics2D.OverlapBoxAll(boxPosition, new Vector2(transform.GetComponent<BoxCollider2D>().size.x - .125f, .125f), 0,groundedMask);
 
         Transform parent = null;
@@ -102,13 +135,35 @@ public class CharMovement : MonoBehaviour
 
         if(collisions.Length > 0)
         {
-            //Debug.Log("grounded");
+            Debug.Log("grounded");
             return true;
         }
         else
         {
-            //Debug.Log("not grounded");
+            Debug.Log("not grounded");
             return false;
+        }
+    }
+
+    int isOnWall()
+    {
+
+        Vector2 boxPositionR = new Vector2(transform.position.x - transform.GetComponent<BoxCollider2D>().size.x / 2 - .0625f, transform.position.y);
+        Vector2 boxPositionL = new Vector2(transform.position.x + transform.GetComponent<BoxCollider2D>().size.x / 2 + .0625f, transform.position.y);
+        Collider2D[] collisionsL = Physics2D.OverlapBoxAll(boxPositionL, new Vector2(.125f, transform.GetComponent<BoxCollider2D>().size.y - .25f), 0, groundedMask);
+        Collider2D[] collisionsR = Physics2D.OverlapBoxAll(boxPositionR, new Vector2(.125f, transform.GetComponent<BoxCollider2D>().size.y - .25f), 0, groundedMask);
+
+        if (collisionsL.Length > 0)
+        {
+            return -1;
+        }
+        else if(collisionsR.Length > 0)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
         }
     }
 
@@ -116,18 +171,55 @@ public class CharMovement : MonoBehaviour
     {
         // Draw a semitransparent blue cube at the transforms position
         Gizmos.color = new Color(1, 0, 0, 0.5f);
-        Gizmos.DrawCube(new Vector3(transform.position.x, transform.position.y - transform.localScale.y/2 - .0625f, 0), new Vector3(transform.GetComponent<BoxCollider2D>().size.x - .125f, .125f, 1));
+        Gizmos.DrawCube(new Vector3(transform.position.x, transform.position.y - transform.GetComponent<BoxCollider2D>().size.y / 2 - .065f, 0), new Vector3(transform.GetComponent<BoxCollider2D>().size.x - .125f, .125f, 1));
+        Vector2 boxPositionR = new Vector2(transform.position.x - transform.GetComponent<BoxCollider2D>().size.x/2 - .0625f, transform.position.y);
+        Vector2 boxPositionL = new Vector2(transform.position.x + transform.GetComponent<BoxCollider2D>().size.x / 2 + .0625f, transform.position.y);
+        Gizmos.DrawCube(boxPositionR, new Vector3(.125f, transform.GetComponent<BoxCollider2D>().size.y - .25f, 1));
+        Gizmos.DrawCube(boxPositionL, new Vector3(.125f, transform.GetComponent<BoxCollider2D>().size.y - .25f, 1));
     }
 
     void Update()
     {
 
         _currentSpeed = _playerStats.CurrentMoveSpeed;
-        moveDirection = Mathf.Lerp(moveDirection, input * _currentSpeed, friction * Time.deltaTime);
-
-        if (isGrounded() && _controller.velocity.y <= 0)
+        if(isGrounded())
         {
+            moveDirection = Mathf.Lerp(moveDirection, input * _currentSpeed, friction * Time.deltaTime);
+            if(_controller.velocity.y <= 0)
+            {
+                _jumps = 0;
+            }
+        }
+        else
+        {
+            moveDirection = Mathf.Lerp(moveDirection, input * _currentSpeed, airFriction * Time.deltaTime);
+            if (!_coyotePostEnabled)
+            {
+                _coyotePostEnabled = true;
+                _coyotePost = 0;
+            }
+            else
+            {
+                if (_coyotePost <= coyoteTime)
+                {
+                    _coyotePost += 1 * Time.deltaTime;
+                }
+            }
+        }
+
+        if (isOnWall() != 0 && _controller.velocity.y <= 0)
+        {
+            if (Mathf.Sign(input) != Mathf.Sign(isOnWall()) && input != 0)
+            {
+                _controller.velocity = new Vector2(moveDirection, 0);
+            }
             _jumps = 0;
+        }
+
+        if(_coyotePre <= coyoteTime)
+        {
+            _coyotePre += 1 * Time.deltaTime;
+            TryJump();
         }
 
         _controller.velocity = new Vector2(moveDirection, _controller.velocity.y);

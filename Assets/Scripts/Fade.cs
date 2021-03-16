7d39af7fba45925b131dff9f5bfc43c61783ffbf
JadenSchneider <schneiderj3185@my.uwstout.edu>
2021-03-16 14:10:26 -0500

    Added a fade to the start of the scene

---
+++ Assets/Scripts/Fade.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fade : MonoBehaviour
+{
+    [SerializeField] Vector3 fadeOutSize;
+    [SerializeField] float fadeDuration;
+
+    private void Start()
+    {
+        StartCoroutine(FadeOut());
+    }
+
+    public IEnumerator FadeOut()
+    {
+        float timeElapsed = 0;
+
+        while (timeElapsed < fadeDuration)
+        {
+            transform.localScale = Vector3.Lerp(transform.localScale, fadeOutSize, timeElapsed / fadeDuration);
+            timeElapsed += Time.deltaTime;
+
+            yield return null;
+        }
+
+        transform.localScale = fadeOutSize;
+    }
+
+    public IEnumerator FadeIn()
+    {
+        float timeElapsed = 0;
+
+        while (timeElapsed < fadeDuration)
+        {
+            transform.localScale = Vector3.Lerp(transform.localScale, Vector3.zero, timeElapsed / fadeDuration);
+            timeElapsed += Time.deltaTime;
+
+            yield return null;
+        }
+
+        transform.localScale = Vector3.zero;
+    }
+}

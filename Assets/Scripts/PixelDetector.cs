709157de8e4b4ff4c7ea5a2acd3828a4322ca173
Steve Datz <sfdatz@live.com>
2021-03-16 15:35:45 -0500

    dfgsdgf

--- Assets/Scripts/PixelDetector.cs
+++ Assets/Scripts/PixelDetector.cs
@@ -40,25 +40,40 @@ namespace Helpers
         {
             Debug.Log($"Evaluating pixels in Sprite: {sprite}");
 
+            Debug.Log($"What are these : x:{sprite.textureRect.x} , y: {sprite.textureRect.y} , width={sprite.textureRect.width}, heigh={sprite.textureRect.height}");
+
+            var x1 = Mathf.FloorToInt(sprite.textureRect.x);
+            var y1 = Mathf.FloorToInt(sprite.textureRect.y);
+            var w1 = Mathf.FloorToInt(sprite.textureRect.width);
+            var h1 = Mathf.FloorToInt(sprite.textureRect.height);
+
+            Debug.Log($"What are these : x:{x1} , y: {y1} , width={w1}, heigh={h1}");
+            Debug.Log($"What are these : x:{(int)sprite.textureRect.x} , y: {(int)sprite.textureRect.y} , width={(int)sprite.textureRect.width}, heigh={(int)sprite.textureRect.height}");
+
+
             Color[] pixels = sprite.texture.GetPixels(
-                       (int)sprite.textureRect.x,
-                       (int)sprite.textureRect.y,
-                       (int)sprite.textureRect.width,
-                       (int)sprite.textureRect.height);
+                       Mathf.FloorToInt(sprite.textureRect.x),
+                        Mathf.FloorToInt(sprite.textureRect.y),
+                        Mathf.FloorToInt(sprite.textureRect.width),
+                        Mathf.FloorToInt(sprite.textureRect.height));
 
             for (int i = 0; i < pixels.Length; ++i)
             {
 
-                int x = i / (int)sprite.textureRect.width;
-                int y = i / (int)sprite.textureRect.height;
+                int x = i / Mathf.FloorToInt(sprite.textureRect.width);
+                int y = i / Mathf.FloorToInt(sprite.textureRect.height);
                 if (pixels[i] == Color.black)
                 {
                     Debug.Log($"COLOR:<color=green> {pixels[i]} </color> Pixel Seen is at Horizontal:  {x} and yHeight:{y}");
                 }
+                else if (pixels[i] == new Color(0.164f, .048f, 0.48f))
+                {
+                    Debug.Log($"COLOR:<color=red> {pixels[i]} </color> Pixel Seen is at Horizontal:  {x} and yHeight:{y}");
+                }
                 else
                 {
 
-                    Debug.Log($"COLOR: {pixels[i]} Pixel Seen is at Horizontal:  {x} and yHeight:{y}");
+                    //Debug.Log($"COLOR: {pixels[i]} Pixel Seen is at Horizontal:  {x} and yHeight:{y}");
                 }
             }
 

f89de9cefeb280da7b2eb926ccc33b5a9454a027
Steve Datz <sfdatz@live.com>
2021-04-04 13:54:14 -0500

    Start of between scene (scoring)

---
+++ Assets/Scripts/Actors/Player/ScoringDummyMovement.cs
@@ -0,0 +1,33 @@
+﻿#pragma warning disable CS0649 // Field is never assigned to, and will always have its default value.
+using System.Collections;
+using UnityEngine;
+
+public class ScoringDummyMovement : MonoBehaviour
+{
+
+    [SerializeField] Transform _endGoal = default;
+    private CharMovement _dummy;
+
+    private void Start()
+    {
+        _dummy = GetComponent<CharMovement>();
+        _dummy.SetInput(1); ///make him run to the right
+    }
+
+    private void Update()
+    {
+        if(Vector3.Distance(_dummy.transform.position, _endGoal.position) < 1)
+        {
+            _dummy.SetInput(0); ///make him stop
+            ///Tell level to load Next
+            StartCoroutine(LoadNext());
+        }
+    }
+
+    IEnumerator LoadNext()
+    {
+        yield return new WaitForSeconds(1);
+        LevelLoader.Instance.LoadNextLevel();
+    }
+
+}
--- Assets/Scripts/World/Level/LevelLoader.cs
+++ Assets/Scripts/World/Level/LevelLoader.cs
@@ -3,21 +3,27 @@ using UnityEngine;
 
 public class LevelLoader : MonoSingleton<LevelLoader>
 {
-    [SerializeField] string _firstLevelName = "";
+    string _firstLevelName = "SteveScene2";
+    int _scoringSceneIndex = 1;
     int _currentSceneIndex = 0;
     Scene _currentScene;
 
     public delegate void SceneIsLoading(bool cond);
     public event SceneIsLoading OnSceneIsLoading;
 
+    private bool _isScoringScene = false;
 
+    private void Start()
+    {
+       
+    }
 
     public void LoadFirstLevel()
     {
         var parameters = new LoadSceneParameters(LoadSceneMode.Single);
         if (_firstLevelName.Equals(""))
         {
-             _currentScene = SceneManager.LoadScene(1, parameters);
+             _currentScene = SceneManager.LoadScene(_scoringSceneIndex+1, parameters);
         }
         else
         {
@@ -28,8 +34,17 @@ public class LevelLoader : MonoSingleton<LevelLoader>
     public void LoadNextLevel()
     {
         OnSceneIsLoading?.Invoke(true);
-        /// Mandatory the victory scene ends the sequence
-        SceneManager.LoadScene(++_currentSceneIndex);
+        if (!_isScoringScene)
+        {
+            _isScoringScene = true;
+            SceneManager.LoadScene(_scoringSceneIndex);
+        }
+        else
+        {
+            _isScoringScene = false;
+            /// Mandatory the victory scene ends the sequence
+            SceneManager.LoadScene(++_currentSceneIndex);
+        }
         OnSceneIsLoading?.Invoke(false);
     }
 }

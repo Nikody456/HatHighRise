adcc59840e4ff9c30a9eaf33bfaad5406b6284d8
Nikody456 <antonnik00@mail.ru>
2021-04-20 23:34:15 +0300

    Moved FootStep function to CharMovement

--- Assets/Scripts/Actors/MovementControllers/PlayerMovement.cs
+++ Assets/Scripts/Actors/MovementControllers/PlayerMovement.cs
@@ -172,9 +172,4 @@ public class PlayerMovement : CharMovement
         Gizmos.DrawCube(boxPositionL, new Vector3(.125f, transform.GetComponent<BoxCollider2D>().size.y - .25f, 1));
     }
 
-    private void FootStep()
-    {
-        AudioManager.Instance.PlaySFX("stepSound");
-    }
-
 }
